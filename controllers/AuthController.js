7ce7b51f896feb3832c42ef8785dcf40fc16b5c9
Gustavo7Vidal <vidalgitgustavo@gmail.com>
2023-01-18 21:20:27 -0300

    finalizando registro e colocando logout

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,3 +1,8 @@
+const User = require('../models/User')
+
+const bcrypt = require('bcryptjs')
+const { unsubscribe } = require('../routes/authRoutes')
+
 module.exports = class AuthController {
 
     static login(req, res) {
@@ -6,5 +11,59 @@ module.exports = class AuthController {
 
     static register(req, res){
         res.render('auth/register')
+    }
+
+    static async registerPost(req,res){
+
+        const {name, email, password, confirmpassword} = req.body
+
+        //password match validation
+        if(password != confirmpassword){
+            req.flash('message', 'A senhas não conferem, tente novamente')
+            res.render('auth/register')
+            return
+        }
+
+        // check if user exists
+        const checkIfUserExist = await User.findOne({where: {email: email}})
+
+        if(checkIfUserExist){
+            req.flash('message', 'O email ja existe')
+            res.render('auth/register')
+
+            return
+        }
+
+        //create password
+        const salt = bcrypt.genSaltSync(10)
+        const hashedPassword = bcrypt.hashSync(password, salt)
+
+        const user = {
+            name,
+            email,
+            password: hashedPassword
+        }
+
+        try{
+            const createdUser = await User.create(user)
+
+            //initialize session
+            req.session.userid = createdUser.id
+
+            req.flash('message', 'Cadastro feito com sucesso')
+
+            req.session.save(() =>{
+                res.redirect('/')
+            })
+
+        } catch(err){
+            console.log(err)
+
+        }
+    }
+
+    static logout(req, res){
+        req.session.destroy()
+        res.redirect('/login')
     }
 }
